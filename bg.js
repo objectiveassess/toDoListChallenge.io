52e5b80044a0ecf9b1ac7dcc525dc60e206fe5d7
objectiveassess <68816945+objectiveassess@users.noreply.github.com>
2021-12-26 19:53:42 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -8,7 +8,7 @@ function handleImgLoad(){
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `images/${imgNumber}.jpg`;
+    image.src = `./images/${imgNumber}.jpg`;
     image.classList.add('bgImage');
     body.prepend(image); // body 맨 앞에 추가
     // image.addEventListener("loadend", handleImgLoad);
